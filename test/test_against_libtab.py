60c641ee937ed131ef20ca53d236f78acfd395f4
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2020-11-09 11:34:22 +0000

    install eigen, flake8

--- test/test_against_libtab.py
+++ test/test_against_libtab.py
@@ -5,18 +5,16 @@ import pytest
 import numpy as np
 
 elements = {
-    "triangle" : [("P", "Lagrange", range(1, 4)), ("N1curl", "Nedelec", range(1, 4)),
-                  ("N1div", "RaviartThomas", range(1, 4))
-                 ],
-    "tetrahedron" : [("P", "Lagrange", range(1, 4)), ("N1curl", "Nedelec", range(1, 3)),
-                    ]
+    "triangle": [("P", "Lagrange", range(1, 4)), ("N1curl", "Nedelec", range(1, 4)),
+                 ("N1div", "RaviartThomas", range(1, 4))],
+    "tetrahedron": [("P", "Lagrange", range(1, 4)), ("N1curl", "Nedelec", range(1, 3))]
 }
 
 
 def to_float(a):
     try:
         return float(a)
-    except:
+    except:  # noqa: E722
         return [to_float(i) for i in a]
 
 
@@ -34,16 +32,5 @@ def test_against_libtab(cell, feast_type, libtab_type, order):
     else:
         basis = element.get_basis_functions()
         sym_result = [[float(subs(b, x, p)[j]) for j in range(element.range_dim) for b in basis] for p in points]
-
-    for b, d in zip(basis, element.dofs):
-        print(b)
-        print(d, d.f)
-
-    for a,b in zip(result, sym_result):
-        print("".join(["=" if np.isclose(i,j) else "E" for i,j in zip(a,b)]))
-    for a,b in zip(result, sym_result):
-        for i,j in zip(a,b):
-            if not np.isclose(i,j):
-                print(i, j, i/j)
 
     assert np.allclose(result, sym_result)
