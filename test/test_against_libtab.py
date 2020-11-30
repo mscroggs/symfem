9183a8ae1efe1cc750b216c4d8de782a34bbc18f
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2020-11-30 09:34:22 +0000

    new libtab test

--- test/test_against_libtab.py
+++ test/test_against_libtab.py
@@ -18,12 +18,27 @@ def to_float(a):
         return [to_float(i) for i in a]
 
 
+def make_lattice(cell, N=3):
+    if cell == "interval":
+        return np.array([[i / N] for i in range(N + 1)])
+    if cell == "triangle":
+        return np.array([[i / N, j / N] for i in range(N + 1) for j in range(N + 1 - i)])
+    if cell == "tetrahedron":
+        return np.array([[i / N, j / N, k / N]
+                         for i in range(N + 1) for j in range(N + 1 - i) for k in range(N + 1 - i - j])
+    if cell == "quadrilateral":
+        return np.array([[i / N, j / N] for i in range(N + 1) for j in range(N + 1)])
+    if cell == "hexahedron":
+        return np.array([[i / N, j / N, k / N]
+                         for i in range(N + 1) for j in range(N + 1) for k in range(N + 1])
+
+
 @pytest.mark.parametrize(("cell", "feast_type", "libtab_type", "order"),
                          [(cell, a, b, order) for cell, ls in elements.items() for a, b, c in ls for order in c])
 def test_against_libtab(cell, feast_type, libtab_type, order):
     element = feast_element(cell, feast_type, order)
-    points = libtab.create_lattice(getattr(libtab.CellType, cell), 3, libtab.LatticeType.equispaced, True)
-    space = getattr(libtab, libtab_type)(cell, order)
+    points = make_lattice(cell)
+    space = libtab.create_element(cell, libtab_type, order)
     result = space.tabulate(0, points)[0]
 
     if element.range_dim == 1:
