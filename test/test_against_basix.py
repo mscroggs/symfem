c375ad3cef92c175d9e4f7ac466c9aa9bc08121e
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2020-12-21 14:26:32 +0000

    libtab -> basix

--- test/test_against_basix.py
+++ test/test_against_basix.py
@@ -1,6 +1,6 @@
 from feast import feast_element
 from feast.symbolic import subs, x
-import libtab
+import basix
 import pytest
 import numpy as np
 
@@ -10,7 +10,9 @@ elements = {
                  ("N1div", "Raviart-Thomas", range(1, 4))],
     "tetrahedron": [("P", "Lagrange", range(1, 4)), ("N1curl", "Nedelec 1st kind H(curl)", range(1, 3)),
                     ("N2curl", "Nedelec 2nd kind H(curl)", range(1, 3)),
-                    ("N1div", "Raviart-Thomas", range(1, 3))]
+                    ("N1div", "Raviart-Thomas", range(1, 3))],
+    "quadrilateral": [("Q", "Lagrange", range(1, 4)), ("dQ", "Discontinuous Lagrange", range(1, 4))],
+    "hexahedron": [("Q", "Lagrange", range(1, 4)), ("dQ", "Discontinuous Lagrange", range(1, 4))]
 }
 
 
@@ -36,12 +38,12 @@ def make_lattice(cell, N=3):
                          for i in range(N + 1) for j in range(N + 1) for k in range(N + 1)])
 
 
-@pytest.mark.parametrize(("cell", "feast_type", "libtab_type", "order"),
+@pytest.mark.parametrize(("cell", "feast_type", "basix_type", "order"),
                          [(cell, a, b, order) for cell, ls in elements.items() for a, b, c in ls for order in c])
-def test_against_libtab(cell, feast_type, libtab_type, order):
+def test_against_basix(cell, feast_type, basix_type, order):
     element = feast_element(cell, feast_type, order)
     points = make_lattice(cell)
-    space = libtab.create_element(libtab_type, cell, order)
+    space = basix.create_element(basix_type, cell, order)
     result = space.tabulate(0, points)[0]
 
     if element.range_dim == 1:
