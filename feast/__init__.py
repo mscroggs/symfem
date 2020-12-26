b8fca958ab7c2cdaacc494920a4cfa715781fbb9
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2020-12-26 13:41:02 +0000

    hide stuff in init

--- feast/__init__.py
+++ feast/__init__.py
@@ -1,35 +1,35 @@
 """FEAST: Finite Element Automatic Symbolic Tabulator."""
-from . import simplex, tp, references
-from .finite_element import FiniteElement
 import os as _os
+from . import simplex as _simplex
+from . import tp as _tp
+from . import references as _references
+from .finite_element import FiniteElement as _FiniteElement
 
 _folder = _os.path.dirname(_os.path.realpath(__file__))
 
 if _os.path.isfile(_os.path.join(_folder, "../VERSION")):
     # If running from folder
-    _v_folder = os.path.join(ave_folder, "..")
+    _v_folder = _os.path.join(_folder, "..")
 else:
     # If running from installation
     _v_folder = _folder
 
-with open(os.path.join(_v_folder, "VERSION")) as f:
-    __version__ = f.read().strip()
-
-version_tuple = tuple(int(i) for i in version.split("."))
+with open(_os.path.join(_v_folder, "VERSION")) as _f:
+    __version__ = _f.read().strip()
 
 _elementlist = {}
 
-for cell_class, module in [("simplex", simplex), ("tp", tp)]:
-    _elementlist[cell_class] = {}
-    for class_name in dir(module):
-        element = getattr(module, class_name)
+for _cell_class, _module in [("simplex", _simplex), ("tp", _tp)]:
+    _elementlist[_cell_class] = {}
+    for _class_name in dir(_module):
+        _element = getattr(_module, _class_name)
         if (
-            isinstance(element, type)
-            and issubclass(element, FiniteElement)
-            and element != FiniteElement
+            isinstance(_element, type)
+            and issubclass(_element, _FiniteElement)
+            and _element != _FiniteElement
         ):
-            for n in element.names:
-                _elementlist[cell_class][n] = element
+            for _n in _element.names:
+                _elementlist[_cell_class][_n] = _element
 
 
 def feast_element(cell_type, element_type, order):
@@ -46,15 +46,15 @@ def feast_element(cell_type, element_type, order):
         The order of the element.
     """
     if cell_type == "interval":
-        reference = references.Interval()
+        reference = _references.Interval()
     elif cell_type == "triangle":
-        reference = references.Triangle()
+        reference = _references.Triangle()
     elif cell_type == "tetrahedron":
-        reference = references.Tetrahedron()
+        reference = _references.Tetrahedron()
     elif cell_type == "quadrilateral":
-        reference = references.Quadrilateral()
+        reference = _references.Quadrilateral()
     elif cell_type == "hexahedron":
-        reference = references.Hexahedron()
+        reference = _references.Hexahedron()
     else:
         raise ValueError(f"Unknown cell type: {cell_type}")
 
