f125d633192097945b2b86e6b1a514425fd44609
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2021-01-01 17:52:21 +0000

    Implement Regge elements

--- symfem/simplex.py
+++ symfem/simplex.py
@@ -11,6 +11,7 @@ from .functionals import (
     TangentIntegralMoment,
     NormalIntegralMoment,
     IntegralMoment,
+    PointInnerProduct
 )
 
 
@@ -236,16 +237,31 @@ class Regge(FiniteElement):
 
     def __init__(self, reference, order):
         assert reference.name in ["triangle", "tetrahedron"]
-        poly = polynomial_set(reference.tdim, reference.tdim ** 2, order)
+        if reference.tdim == 2:
+            poly = [(p[0], p[1], p[1], p[2])
+                    for p in polynomial_set(reference.tdim, 3, order)]
+        if reference.tdim == 3:
+            poly = [(p[0], p[1], p[3], p[1], p[2], p[4], p[3], p[4], p[5])
+                    for p in polynomial_set(reference.tdim, 6, order)]
 
         dofs = []
-        dofs += make_integral_moment_dofs(
-            reference,
-            facets=(NormalIntegralMoment, DiscontinuousLagrange, order),
-            cells=(IntegralMoment, VectorDiscontinuousLagrange, order - 2),
-        )
+        for edim in range(1, 4):
+            for vs in reference.sub_entities(edim):
+                entity = reference.sub_entity_types[edim](
+                    vertices=tuple(reference.reference_vertices[i] for i in vs)
+                )
+                for i in product(range(1, order + 2), repeat=edim):
+                    if sum(i) < order + 2:
+                        for edge in entity.edges:
+                            tangent = [b - a for a, b in zip(entity.vertices[edge[0]],
+                                                             entity.vertices[edge[1]])]
+                            dofs.append(PointInnerProduct(
+                                tuple(o + sum(sympy.Rational(a[j] * b, order + 2)
+                                              for a, b in zip(entity.axes, i))
+                                      for j, o in enumerate(entity.origin)),
+                                tangent, entity_dim=edim))
 
         super().__init__(reference, poly, dofs, reference.tdim, reference.tdim)
 
     names = ["Regge"]
-    min_order = 1
+    min_order = 0
