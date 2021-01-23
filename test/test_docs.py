ef29bb411d1594f19d1ec85e14baeeafb7dcfaf5
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2021-01-23 16:44:55 +0000

    test that code in docs is correct

--- test/test_docs.py
+++ test/test_docs.py
@@ -1,4 +1,40 @@
 import symfem
+from io import StringIO
+import sys
+import os
+import re
+import pytest
+
+code = False
+output = False
+lines = []
+doc_data = []
+outputlines = []
+codelines = []
+
+with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "../docs/index.rst")) as f:
+    for line in f:
+        if line.strip() == ".. code-block:: python":
+            code = True
+            output = False
+            codelines = []
+            outputlines = []
+        elif code and line.strip() == "::":
+            output = True
+            code = False
+        elif code or output:
+            if line.strip() != "":
+                if line.startswith("    "):
+                    if code:
+                        assert not output
+                        codelines.append(line.strip("\n")[4:])
+                    else:
+                        assert output
+                        outputlines.append(line.strip("\n")[4:])
+                else:
+                    output = False
+                    code = False
+                    doc_data.append(("\n".join(codelines), "\n".join(outputlines)))
 
 
 def test_available_references():
@@ -25,3 +61,22 @@ def test_available_elements():
     supported = set([i.strip() for i in supported.split(",")])
 
     assert set(symfem._elementmap.keys()) == supported
+
+
+@pytest.mark.parametrize("script, output", doc_data)
+def test_snippets(script, output):
+    old_stdout = sys.stdout
+    redirected_output = sys.stdout = StringIO()
+    exec(script)
+    sys.stdout = old_stdout
+
+    actual_output = redirected_output.getvalue().strip()
+    actual_output = re.sub(r"at 0x[^\>]+\>", "at 0x{ADDRESS}>", actual_output)
+    print("------------------------------")
+    print(script)
+    print("------------------------------")
+    print(output)
+    print("------------------------------")
+    print(actual_output)
+    print("------------------------------")
+    assert actual_output == output
