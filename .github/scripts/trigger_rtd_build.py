cd674798f37f497e52405dcf8146308b3de95b5a
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2021-03-02 08:17:47 +0000

    bump version number to cause doc rebuild

--- .github/scripts/trigger_rtd_build.py
+++ .github/scripts/trigger_rtd_build.py
@@ -1,7 +1,7 @@
 import requests
 import sys
 
-URL = 'https://readthedocs.org/api/v3/projects/symfem/versions/stable/builds/'
+URL = 'https://readthedocs.org/api/v3/projects/symfem/versions/latest/builds/'
 TOKEN = sys.argv[-1]
 HEADERS = {'Authorization': f'token {TOKEN}'}
 response = requests.post(URL, headers=HEADERS)
