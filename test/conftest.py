8f0e0c52b1c406039e8ff10bc5b59aee3efdf3cd
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2021-03-03 09:58:20 +0000

    test against more Basix spaces

--- test/conftest.py
+++ test/conftest.py
@@ -6,6 +6,7 @@ import pytest
 def pytest_addoption(parser):
     parser.addoption("--elements-to-test", action="store", default="ALL")
     parser.addoption("--cells-to-test", action="store", default="ALL")
+    parser.addoption("--has-basix", action="store", default="0")
 
 
 @pytest.fixture
@@ -22,3 +23,9 @@ def cells_to_test(request):
     if data == "ALL":
         return "ALL"
     return data.split(",")
+
+
+@pytest.fixture
+def has_basix(request):
+    data = request.config.getoption("--has-basix")
+    return data == "1"
