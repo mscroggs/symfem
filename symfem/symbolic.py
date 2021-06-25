73ab459c6e4898d2f45e7642ae430b200147dde4
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2021-06-25 16:57:37 +0100

    Start working on new symbolic poynomials

--- symfem/symbolic.py
+++ symfem/symbolic.py
@@ -2,10 +2,71 @@
 
 import sympy
 
+
+class Polynomial:
+    def __init__(self, xpow=0, ypow=0, zpow=0, negative=False):
+        self._x = xpow
+        self._y = ypow
+        self._z = zpow
+        self._negative = negative
+
+    def to_sympy(self):
+        _x = [sympy.Symbol("x"), sympy.Symbol("y"), sympy.Symbol("z")]
+        if self._negative:
+            return _x[0] ** self._x * _x[1] ** self._y * _x[2] ** self._z
+        else:
+            return -_x[0] ** self._x * _x[1] ** self._y * _x[2] ** self._z
+
+    def __eq__(self, other):
+        if isinstance(other, Polynomial):
+            return self._x == other._x and self._y == other._y and self._z == other._z and self._negative == other._negative
+        return self.to_sympy() == other
+
+    def __mul__(self, other):
+        if isinstance(other, Polynomial):
+            return Polynomial(self._x + other._x, self._y + other._y, self._z + other._z, self._negative)
+        return self.to_sympy() * other
+
+    def __rmul__(self, other):
+        return self.__mul__(other)
+
+    def __truediv__(self, other):
+        if isinstance(other, Polynomial):
+            return Polynomial(self._x - other._x, self._y - other._y, self._z - other._z, self._negative)
+        return self.to_sympy() / other
+
+    def __rtruediv__(self, other):
+        if isinstance(other, Polynomial):
+            return Polynomial(other._x - self._x, other._y - self._y, other._z - self._z, self._negative)
+        return other / self.to_sympy()
+
+    def __pow__(self, power):
+        return Polynomial(self._x * power, self._y * power, self._z * power, self._negative)
+
+    def __add__(self, other):
+        return self.to_sympy() + other
+
+    def __radd__(self, other):
+        return self.__add__(other)
+
+    def __sub__(self, other):
+        return self.to_sympy() - other
+
+    def __rsub__(self, other):
+        return other - self.to_sympy()
+
+    def __neg__(self):
+        return Polynomial(self._x, self._y, self._z, not self._negative)
+
+    def __getattr__(self, attr):
+        return getattr(self.to_sympy(), attr)
+
+x = [Polynomial(xpow=1), Polynomial(ypow=1), Polynomial(zpow=1)]
+
 t = [sympy.Symbol("t0"), sympy.Symbol("t1"), sympy.Symbol("t2")]
-x = [sympy.Symbol("x"), sympy.Symbol("y"), sympy.Symbol("z")]
-zero = sympy.Integer(0)
-one = sympy.Integer(1)
+
+zero = 0
+one = 1
 
 _dummy = [sympy.Symbol("symbolicpyDUMMYx"), sympy.Symbol("symbolicpyDUMMYy"),
           sympy.Symbol("symbolicpyDUMMYz")]
@@ -13,6 +74,16 @@ _dummy = [sympy.Symbol("symbolicpyDUMMYx"), sympy.Symbol("symbolicpyDUMMYy"),
 
 def subs(f, vars, values):
     """Substitute values into a sympy expression."""
+
+    if isinstance(f, Polynomial):
+        return subs(f.to_sympy(), vars, values)
+    for i, j in enumerate(vars):
+        if isinstance(j, Polynomial):
+            return subs(f, vars[:i] + [j.to_sympy()] + vars[i + 1: ], values)
+    for i, j in enumerate(values):
+        if isinstance(j, Polynomial):
+            return subs(f, vars, values[:i] + [j.to_sympy()] + values[i + 1: ])
+
     if isinstance(f, PiecewiseFunction):
         return f.evaluate(values)
     try:
@@ -21,6 +92,9 @@ def subs(f, vars, values):
         pass
     if isinstance(vars, sympy.Symbol):
         return (zero + f).subs(vars, values)
+
+    if isinstance(f, int):
+        return f
 
     if len(values) == 1:
         return f.subs(vars[0], values[0])
