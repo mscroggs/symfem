b1910d35216890fb2057b0e4605dc1b492fffcf3
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2021-09-03 11:23:42 +0100

    stop trying to make negative order spaces

--- symfem/elements/nedelec_prism.py
+++ symfem/elements/nedelec_prism.py
@@ -7,7 +7,6 @@ from ..polynomials import (Hcurl_polynomials, polynomial_set_1d,
 from ..functionals import TangentIntegralMoment, IntegralMoment, IntegralAgainst
 from ..symbolic import x
 from .lagrange import Lagrange, VectorLagrange
-from .dpc import DPC, VectorDPC
 from .q import RaviartThomas as QRT
 
 
@@ -37,20 +36,19 @@ class Nedelec(CiarletElement):
         triangle = create_reference("triangle")
         interval = create_reference("interval")
 
-        space1 = VectorLagrange(triangle, order - 2, variant)
-        space2 = Lagrange(interval, order - 2, variant)
+        if order >= 2:
+            space1 = VectorLagrange(triangle, order - 2, variant)
+            space2 = Lagrange(interval, order - 2, variant)
 
-        if order > 2:
-            raise NotImplementedError()
-        # TODO: correct these for order > 2
-        for i in range(space1.space_dim):
-            for j in range(space2.space_dim):
-                f = (space2.get_basis_function(j) * space1.get_basis_function(i)[0],
-                     space2.get_basis_function(j) * space1.get_basis_function(i)[1],
-                     0)
-                dofs.append(IntegralAgainst(reference, f, entity=(3, 0), mapping="covariant"))
-
-        print(len(poly), len(dofs))
+            if order > 2:
+                raise NotImplementedError()
+            # TODO: correct these for order > 2
+            for i in range(space1.space_dim):
+                for j in range(space2.space_dim):
+                    f = (space2.get_basis_function(j) * space1.get_basis_function(i)[0],
+                         space2.get_basis_function(j) * space1.get_basis_function(i)[1],
+                         0)
+                    dofs.append(IntegralAgainst(reference, f, entity=(3, 0), mapping="covariant"))
 
         super().__init__(reference, order, poly, dofs, reference.tdim, reference.tdim)
         self.variant = variant
