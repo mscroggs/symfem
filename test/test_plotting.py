02d4fcc57aff939b8fd346277af8ed700d0cda9d
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2022-07-24 11:28:12 +0100

    write test plots to _temp dir, not into test folder

--- test/test_plotting.py
+++ test/test_plotting.py
@@ -6,20 +6,21 @@ import sympy
 import symfem
 import symfem.plotting
 
-dir = os.path.dirname(os.path.realpath(__file__))
+folder = os.path.join(os.path.dirname(os.path.realpath(__file__)), "../_temp")
+os.system(f"mkdir {folder}")
 
 
 def compile_tex(filename):
-    with open(os.path.join(dir, filename)) as f:
+    with open(os.path.join(folder, filename)) as f:
         content = f.read()
     filename2 = filename.split(".")[0] + "-with-preamble.tex"
-    with open(os.path.join(dir, filename2), "w") as f:
+    with open(os.path.join(folder, filename2), "w") as f:
         f.write("\\documentclass{standalone}\n")
         f.write("\\usepackage{tikz}\n")
         f.write("\\begin{document}\n")
         f.write(content)
         f.write("\\end{document}\n")
-    assert os.system(f"cd {dir} && pdflatex -halt-on-error {filename2} > /dev/null") == 0
+    assert os.system(f"cd {folder} && pdflatex -halt-on-error {filename2} > /dev/null") == 0
 
 
 def test_plot_line():
@@ -27,8 +28,8 @@ def test_plot_line():
     p.add_line((sympy.Integer(0), sympy.Integer(0)),
                (sympy.Integer(1), sympy.Integer(1)), "blue")
     for ext in ["svg", "png", "tex"]:
-        p.save(os.path.join(dir, f"test-output-test_plot_line.{ext}"))
-    compile_tex("test-output-test_plot_line.tex")
+        p.save(os.path.join(folder, f"test_plot_line.{ext}"))
+    compile_tex("test_plot_line.tex")
 
 
 def test_plot_arrow():
@@ -36,16 +37,16 @@ def test_plot_arrow():
     p.add_arrow((sympy.Integer(0), sympy.Integer(0)),
                 (sympy.Integer(1), sympy.Integer(1)), "orange")
     for ext in ["svg", "png", "tex"]:
-        p.save(os.path.join(dir, f"test-output-test_plot_arrow.{ext}"))
-    compile_tex("test-output-test_plot_arrow.tex")
+        p.save(os.path.join(folder, f"test_plot_arrow.{ext}"))
+    compile_tex("test_plot_arrow.tex")
 
 
 def test_plot_ncircle():
     p = symfem.plotting.Picture()
     p.add_ncircle((sympy.Integer(0), sympy.Integer(0)), 5, "green")
     for ext in ["svg", "png", "tex"]:
-        p.save(os.path.join(dir, f"test-output-test_plot_ncircle.{ext}"))
-    compile_tex("test-output-test_plot_ncircle.tex")
+        p.save(os.path.join(folder, f"test_plot_ncircle.{ext}"))
+    compile_tex("test_plot_ncircle.tex")
 
 
 def test_plot_fill():
@@ -55,8 +56,8 @@ def test_plot_fill():
         (sympy.Integer(1), sympy.Rational(1, 2)), (sympy.Rational(1, 2), sympy.Integer(0))
     ), "purple", 0.5)
     for ext in ["svg", "png", "tex"]:
-        p.save(os.path.join(dir, f"test-output-test_plot_fill.{ext}"))
-    compile_tex("test-output-test_plot_fill.tex")
+        p.save(os.path.join(folder, f"test_plot_fill.{ext}"))
+    compile_tex("test_plot_fill.tex")
 
 
 @pytest.mark.parametrize("reference", [
@@ -85,8 +86,8 @@ def test_dof_diagrams_lagrange(reference, degree):
     e = symfem.create_element(reference, "P", degree)
     for ext in ["svg", "png", "tex"]:
         e.plot_dof_diagram(os.path.join(
-            dir, f"test-output-test_dof_diagrams_lagrange-{reference}-{degree}.{ext}"))
-    compile_tex(f"test-output-test_dof_diagrams_lagrange-{reference}-{degree}.tex")
+            folder, f"test_dof_diagrams_lagrange-{reference}-{degree}.{ext}"))
+    compile_tex(f"test_dof_diagrams_lagrange-{reference}-{degree}.tex")
 
 
 @pytest.mark.parametrize("reference", [
@@ -101,8 +102,8 @@ def test_dof_diagrams_raviart_thomas(reference, degree):
         e = symfem.create_element(reference, "RT", degree)
     for ext in ["svg", "png", "tex"]:
         e.plot_dof_diagram(os.path.join(
-            dir, f"test-output-test_dof_diagrams_raviart_thomas-{reference}-{degree}.{ext}"))
-    compile_tex(f"test-output-test_dof_diagrams_raviart_thomas-{reference}-{degree}.tex")
+            folder, f"test_dof_diagrams_raviart_thomas-{reference}-{degree}.{ext}"))
+    compile_tex(f"test_dof_diagrams_raviart_thomas-{reference}-{degree}.tex")
 
 
 @pytest.mark.parametrize("reference", [
@@ -113,8 +114,8 @@ def test_function_plots_lagrange(reference, degree):
     e = symfem.create_element(reference, "P", degree)
     for ext in ["svg", "png", "tex"]:
         e.plot_basis_function(0, os.path.join(
-            dir, f"test-output-test_function_plots_lagrange-{reference}-{degree}.{ext}"))
-    compile_tex(f"test-output-test_function_plots_lagrange-{reference}-{degree}.tex")
+            folder, f"test_function_plots_lagrange-{reference}-{degree}.{ext}"))
+    compile_tex(f"test_function_plots_lagrange-{reference}-{degree}.tex")
 
 
 @pytest.mark.parametrize("reference", [
@@ -129,8 +130,8 @@ def test_function_plots_raviart_thomas(reference, degree):
         e = symfem.create_element(reference, "RT", degree)
     for ext in ["svg", "png", "tex"]:
         e.plot_basis_function(0, os.path.join(
-            dir, f"test-output-test_function_plots_raviart_thomas-{reference}-{degree}.{ext}"))
-    compile_tex(f"test-output-test_function_plots_raviart_thomas-{reference}-{degree}.tex")
+            folder, f"test_function_plots_raviart_thomas-{reference}-{degree}.{ext}"))
+    compile_tex(f"test_function_plots_raviart_thomas-{reference}-{degree}.tex")
 
 
 @pytest.mark.parametrize("reference", [
@@ -140,8 +141,8 @@ def test_function_plots_piecewise_scalar(reference):
     e = symfem.create_element(reference, "P1-iso-P2", 1)
     for ext in ["svg", "png", "tex"]:
         e.plot_basis_function(0, os.path.join(
-            dir, f"test-output-test_function_plots_piecewise_scalar-{reference}.{ext}"))
-    compile_tex(f"test-output-test_function_plots_piecewise_scalar-{reference}.tex")
+            folder, f"test_function_plots_piecewise_scalar-{reference}.{ext}"))
+    compile_tex(f"test_function_plots_piecewise_scalar-{reference}.tex")
 
 
 @pytest.mark.parametrize("reference", [
@@ -151,8 +152,8 @@ def test_function_plots_piecewise_vector(reference):
     e = symfem.create_element(reference, "Guzman-Neilan", 1)
     for ext in ["svg", "png", "tex"]:
         e.plot_basis_function(0, os.path.join(
-            dir, f"test-output-test_function_plots_piecewise_vector-{reference}.{ext}"))
-    compile_tex(f"test-output-test_function_plots_piecewise_vector-{reference}.tex")
+            folder, f"test_function_plots_piecewise_vector-{reference}.{ext}"))
+    compile_tex(f"test_function_plots_piecewise_vector-{reference}.tex")
 
 
 @pytest.mark.parametrize("n", [4, 6])
@@ -160,8 +161,8 @@ def test_function_plots_dual(n):
     e = symfem.create_element(f"dual polygon({n})", "dual", 1)
     for ext in ["svg", "png", "tex"]:
         e.plot_basis_function(0, os.path.join(
-            dir, f"test-output-test_function_plots_dual-{n}.{ext}"))
-    compile_tex(f"test-output-test_function_plots_dual-{n}.tex")
+            folder, f"test_function_plots_dual-{n}.{ext}"))
+    compile_tex(f"test_function_plots_dual-{n}.tex")
 
 
 @pytest.mark.parametrize("n", [4, 6])
@@ -169,8 +170,8 @@ def test_function_plots_bc(n):
     e = symfem.create_element(f"dual polygon({n})", "BC", 1)
     for ext in ["svg", "png", "tex"]:
         e.plot_basis_function(0, os.path.join(
-            dir, f"test-output-test_function_plots_bc-{n}.{ext}"))
-    compile_tex(f"test-output-test_function_plots_bc-{n}.tex")
+            folder, f"test_function_plots_bc-{n}.{ext}"))
+    compile_tex(f"test_function_plots_bc-{n}.tex")
 
 
 @pytest.mark.parametrize("reference", [
@@ -183,5 +184,5 @@ def test_plot_reference(reference):
     rname = reference.replace("(", "").replace(")", "").replace(" ", "_")
     for ext in ["svg", "png", "tex"]:
         r.plot_entity_diagrams(os.path.join(
-            dir, f"test-output-test_plot_reference-{rname}.{ext}"))
-    compile_tex(f"test-output-test_plot_reference-{rname}.tex")
+            folder, f"test_plot_reference-{rname}.{ext}"))
+    compile_tex(f"test_plot_reference-{rname}.tex")
