ece0c40ce0e8d1af9d67c0444c1408b52828a4f9
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2022-10-18 14:34:24 +0100

    flake8

--- symfem/elements/huang_zhang.py
+++ symfem/elements/huang_zhang.py
@@ -1,6 +1,6 @@
 """Huang-Zhang elements on a triangle.
 
-This element's definition appears in https://doi.org/10.1007/s11464-011-0094-0 
+This element's definition appears in https://doi.org/10.1007/s11464-011-0094-0
 (Huang, Zhang, 2011)
 """
 
@@ -12,32 +12,9 @@ from ..finite_element import CiarletElement
 from ..functionals import ListOfFunctionals, PointEvaluation
 from ..functions import FunctionInput
 from ..references import Reference
-from ..symbols import AxisVariablesNotSingle, x
-from ..functions import ScalarFunction, VectorFunction
+from ..symbols import x
+from ..functions import ScalarFunction
 
-def polynomial_set_1d_ZH(
-         dim: int, variant: int, variables: AxisVariablesNotSingle = x
-) -> typing.List[ScalarFunction]:
-    """Huang-Zhang ."""
-    order = 2;
-    if dim == 1:
-        raise RuntimeError('The Haung-Zhang polynomial space is not implemented for dimension {}'.format(dim))
-    if dim == 2:
-        if variant == 0:
-            return [
-                ScalarFunction(variables[0] ** i * variables[1] ** j)
-                for j in range(order + 1)
-                for i in range(order)
-            ]
-        elif variant == 1:
-            return [
-                ScalarFunction(variables[0] ** i * variables[1] ** j)
-                for j in range(order)
-                for i in range(order+1)
-            ]
-
-    if dim == 3:
-        raise RuntimeError('The Haung-Zhang polynomial space is not implemented for dimension {}'.format(dim))
 
 class HuangZhang(CiarletElement):
     """Huang-Zhang finite element."""
@@ -48,36 +25,42 @@ class HuangZhang(CiarletElement):
         Args:
             reference: The reference element
             order: The polynomial order
+            variant: X or Y variant
         """
         assert reference.name == "quadrilateral"
-
         assert order == 2
-        
+
         half = sympy.Rational(1, 2)
         if variant == "X":
             dofs: ListOfFunctionals = [
-                PointEvaluation(reference, (0,0), entity=(0, 0)),
-                PointEvaluation(reference, (0,1), entity=(0, 1)),
-                PointEvaluation(reference, (1,1), entity=(0, 2)),
+                PointEvaluation(reference, (0, 0), entity=(0, 0)),
+                PointEvaluation(reference, (0, 1), entity=(0, 1)),
+                PointEvaluation(reference, (1, 1), entity=(0, 2)),
                 PointEvaluation(reference, (1, 0), entity=(0, 3)),
-                PointEvaluation(reference, (half,1), entity=(1, 1)),
-                PointEvaluation(reference, (half,0), entity=(1, 3))
+                PointEvaluation(reference, (half, 1), entity=(1, 1)),
+                PointEvaluation(reference, (half, 0), entity=(1, 3))
             ]
 
-            poly: typing.List[FunctionInput] = []
-            poly += polynomial_set_1d_ZH(reference.tdim,1)
+            poly: typing.List[FunctionInput] = [
+                ScalarFunction(x[0] ** i * x[1] ** j)
+                for j in range(order)
+                for i in range(order+1)
+            ]
         elif variant == "Y":
             dofs: ListOfFunctionals = [
-                PointEvaluation(reference, (0,0), entity=(0, 0)),
-                PointEvaluation(reference, (0,1), entity=(0, 1)),
-                PointEvaluation(reference, (1,1), entity=(0, 2)),
+                PointEvaluation(reference, (0, 0), entity=(0, 0)),
+                PointEvaluation(reference, (0, 1), entity=(0, 1)),
+                PointEvaluation(reference, (1, 1), entity=(0, 2)),
                 PointEvaluation(reference, (1, 0), entity=(0, 3)),
-                PointEvaluation(reference, (0,1+half), entity=(1, 0)),
-                PointEvaluation(reference, (1,1+half), entity=(1, 2))
+                PointEvaluation(reference, (0, 1 + half), entity=(1, 0)),
+                PointEvaluation(reference, (1, 1 + half), entity=(1, 2))
             ]
 
-            poly: typing.List[FunctionInput] = []
-            poly += polynomial_set_1d_ZH(reference.tdim,0)
+            poly: typing.List[FunctionInput] = [
+                ScalarFunction(x[0] ** i * x[1] ** j)
+                for j in range(order + 1)
+                for i in range(order)
+            ]
         super().__init__(reference, order, poly, dofs, reference.tdim, 1)
 
     names = ["Huang-Zhang", "HZ"]
