6bd06e02771567028a3f22e6490c566e53a65556
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2022-12-07 19:23:27 +0000

    make x and y variants into one element

--- symfem/elements/huang_zhang.py
+++ symfem/elements/huang_zhang.py
@@ -9,66 +9,62 @@ import typing
 import sympy
 
 from ..finite_element import CiarletElement
-from ..functionals import ListOfFunctionals, PointEvaluation
+from ..functionals import ListOfFunctionals, DotPointEvaluation
 from ..functions import FunctionInput
 from ..references import Reference
 from ..symbols import x
-from ..functions import ScalarFunction
+from ..functions import VectorFunction
 
 
 class HuangZhang(CiarletElement):
     """Huang-Zhang finite element."""
 
-    def __init__(self, reference: Reference, order: int, variant: str):
+    def __init__(self, reference: Reference, order: int):
         """Create the element.
 
         Args:
             reference: The reference element
             order: The polynomial order
-            variant: X or Y variant
         """
         assert reference.name == "quadrilateral"
-        assert order == 2
+        assert order == 1
 
         half = sympy.Rational(1, 2)
         dofs: ListOfFunctionals = []
         poly: typing.List[FunctionInput] = []
+        poly += [
+            VectorFunction([x[0] ** i * x[1] ** j, 0])
+            for j in range(2)
+            for i in range(3)
+        ]
+        poly += [
+            VectorFunction([0, x[0] ** i * x[1] ** j])
+            for j in range(3)
+            for i in range(2)
+        ]
 
-        if variant == "X":
-            dofs = [
-                PointEvaluation(reference, (0, 0), entity=(0, 0)),
-                PointEvaluation(reference, (0, 1), entity=(0, 1)),
-                PointEvaluation(reference, (1, 1), entity=(0, 2)),
-                PointEvaluation(reference, (1, 0), entity=(0, 3)),
-                PointEvaluation(reference, (half, 1), entity=(1, 1)),
-                PointEvaluation(reference, (half, 0), entity=(1, 3))
-            ]
+        dofs += [
+            DotPointEvaluation(reference, (0, 0), (1, 0), entity=(0, 0)),
+            DotPointEvaluation(reference, (0, 1), (1, 0), entity=(0, 1)),
+            DotPointEvaluation(reference, (1, 1), (1, 0), entity=(0, 2)),
+            DotPointEvaluation(reference, (1, 0), (1, 0), entity=(0, 3)),
+            DotPointEvaluation(reference, (half, 1), (1, 0), entity=(1, 1)),
+            DotPointEvaluation(reference, (half, 0), (1, 0), entity=(1, 3))
+        ]
 
-            poly = [
-                ScalarFunction(x[0] ** i * x[1] ** j)
-                for j in range(order)
-                for i in range(order+1)
-            ]
-        elif variant == "Y":
-            dofs = [
-                PointEvaluation(reference, (0, 0), entity=(0, 0)),
-                PointEvaluation(reference, (0, 1), entity=(0, 1)),
-                PointEvaluation(reference, (1, 1), entity=(0, 2)),
-                PointEvaluation(reference, (1, 0), entity=(0, 3)),
-                PointEvaluation(reference, (0, 1 + half), entity=(1, 0)),
-                PointEvaluation(reference, (1, 1 + half), entity=(1, 2))
-            ]
+        dofs += [
+            DotPointEvaluation(reference, (0, 0), (0, 1), entity=(0, 0)),
+            DotPointEvaluation(reference, (0, 1), (0, 1), entity=(0, 1)),
+            DotPointEvaluation(reference, (1, 1), (0, 1), entity=(0, 2)),
+            DotPointEvaluation(reference, (1, 0), (0, 1), entity=(0, 3)),
+            DotPointEvaluation(reference, (0, half), (0, 1), entity=(1, 0)),
+            DotPointEvaluation(reference, (1, half), (0, 1), entity=(1, 2))
+        ]
 
-            poly = [
-                ScalarFunction(x[0] ** i * x[1] ** j)
-                for j in range(order + 1)
-                for i in range(order)
-            ]
-
-        super().__init__(reference, order, poly, dofs, reference.tdim, 1)
+        super().__init__(reference, order, poly, dofs, reference.tdim, reference.tdim)
 
     names = ["Huang-Zhang", "HZ"]
     references = ["quadrilateral"]
-    min_order = 2
-    max_order = 2
+    min_order = 1
+    max_order = 1
     continuity = "H(div)"
