18420800c72431b23c77f41004907c35c8dce19f
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2023-05-25 08:13:19 +0100

    style

--- symfem/elements/alfeld_sorokina.py
+++ symfem/elements/alfeld_sorokina.py
@@ -9,7 +9,7 @@ import typing
 import sympy
 
 from ..finite_element import CiarletElement
-from ..functionals import PointDivergenceEvaluation, ListOfFunctionals, DotPointEvaluation
+from ..functionals import DotPointEvaluation, ListOfFunctionals, PointDivergenceEvaluation
 from ..functions import FunctionInput, VectorFunction
 from ..piecewise_functions import PiecewiseFunction
 from ..references import Reference
