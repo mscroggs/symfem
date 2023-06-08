e50f33869d9a9d1af5ae4c0adcfb012d7f05bc1e
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2023-06-08 13:57:58 +0100

    Restore old inv version after overwriting it in test

--- test/test_caching.py
+++ test/test_caching.py
@@ -12,11 +12,14 @@ def test_cache():
     def a(*args, **kwargs):
         raise RuntimeError()
 
+    oldinv = sympy.Matrix.inv
     sympy.Matrix.inv = a
 
     with pytest.raises(RuntimeError):
         e.get_dual_matrix(caching=False, inverse=True)
 
     m2 = e.get_dual_matrix(inverse=True)
+
+    sympy.Matrix.inv = oldinv
 
     assert allequal(m1, m2)
