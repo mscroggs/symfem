b9c16ef0b2d9348e8f3f26ac060a05f0c1ad0eb6
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2023-12-18 11:24:33 +0000

    correct script

--- prepare_release.py
+++ prepare_release.py
@@ -8,7 +8,7 @@ content = parts[0]
 for p in parts[1:]:
     if not p.startswith("http"):
         content += "https://raw.githubusercontent.com/mscroggs/symfem/main/"
-    content += "p"
+    content += "](" + p
 
 with open("README.md", "w") as f:
     f.write(content)
