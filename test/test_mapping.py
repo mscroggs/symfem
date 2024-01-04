4d1a323693b7a97fa18b43784dd6a94bfecb8e9e
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2024-01-04 17:57:17 +0000

    Add get_mapping function (#263)
    * Add get_mapping function
    
    * typo
    
    * add test
    
    * [] not ()
    
    * isort

--- test/test_mapping.py
+++ test/test_mapping.py
@@ -56,3 +56,25 @@ def test_push_forward(
         assert allequal(e.map_to_cell(vertices), e2.get_basis_functions())
     except MappingNotImplemented:
         pytest.xfail("Mapping not implemented for this element.")
+
+
+@pytest.mark.parametrize("name, inverse, transpose, mapping", [
+    ("identity", False, False, symfem.mappings.identity),
+    ("l2", False, False, symfem.mappings.l2),
+    ("covariant", False, False, symfem.mappings.covariant),
+    ("contravariant", False, False, symfem.mappings.contravariant),
+    ("double_covariant", False, False, symfem.mappings.double_covariant),
+    ("double_contravariant", False, False, symfem.mappings.double_contravariant),
+    ("identity", True, True, symfem.mappings.identity_inverse_transpose),
+    ("l2", True, True, symfem.mappings.l2_inverse_transpose),
+    ("covariant", True, True, symfem.mappings.covariant_inverse_transpose),
+    ("contravariant", True, True, symfem.mappings.contravariant_inverse_transpose),
+    ("identity", True, False, symfem.mappings.identity_inverse),
+    ("l2", True, False, symfem.mappings.l2_inverse),
+    ("covariant", True, False, symfem.mappings.covariant_inverse),
+    ("contravariant", True, False, symfem.mappings.contravariant_inverse),
+    ("double_covariant", True, False, symfem.mappings.double_covariant_inverse),
+    ("double_contravariant", True, False, symfem.mappings.double_contravariant_inverse),
+])
+def test_get_mapping(name, inverse, transpose, mapping):
+    assert symfem.mappings.get_mapping(name, inverse=inverse, transpose=transpose) == mapping
