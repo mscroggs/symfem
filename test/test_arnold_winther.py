ffee3bd4cd55594da01aa8cc65fba4ad400f91ad
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2024-10-10 16:19:46 +0100

    Correct nonconforming AW polyset

--- test/test_arnold_winther.py
+++ test/test_arnold_winther.py
@@ -3,8 +3,20 @@
 import pytest
 
 from symfem import create_element
+from symfem.symbols import x
 
 
 @pytest.mark.parametrize("order", range(3, 7))
 def test_create(order):
     create_element("triangle", "Arnold-Winther", order)
+
+
+def test_nc_polyset():
+    e = create_element("triangle", "nonconforming AW", 2)
+
+    for p in e.get_polynomial_basis():
+        assert p[1, 1].as_sympy().subs(x[1], 0).diff(x[1]).diff(x[1]) == 0
+        assert p[0, 0].as_sympy().subs(x[0], 0).diff(x[0]).diff(x[0]) == 0
+        assert (
+            p[0, 0].as_sympy() + p[0, 1].as_sympy() + p[1, 0].as_sympy() + p[1, 1].as_sympy()
+        ).subs(x[0], 1 - x[1]).expand().diff(x[1]).diff(x[1]) == 0
